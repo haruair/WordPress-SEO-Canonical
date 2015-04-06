0842e514022f8c32597d0d3198cec766fb2033c6
Edward Kim <onward.edkim@gmail.com>
2015-04-07 02:22:39 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+WordPress SEO Canonical
+=======================
+This simple plugin helps your canonical URL update. This plugin has a dependency of other plugins below:
+
+- [WordPress SEO](https://wordpress.org/plugins/wordpress-seo/)
+- [FeedWordPress](https://wordpress.org/plugins/feedwordpress/)
---
+++ wordpress-seo-canonical.php
@@ -0,0 +1,23 @@
+<?php
+/* 
+Plugin Name: WordPress SEO Canonical For FeedWordPress
+Plugin URI: https://github.com/haruair/WordPress-SEO-Canonical
+Description: Override Canoncial URL Meta Tag for FeedWordPress. This plugin required WordPress SEO Plugin.
+Version: 0.1
+Author: Edward Kim <onward.edkim@gmail.com>
+Author URI: http://haruair.com
+License: GPL v2
+*/
+
+add_filter('wpseo_canonical', 'weirdmeetup_wordpress_SEO_canonical_filter');
+
+function weirdmeetup_wordpress_SEO_canonical_filter($canonical)
+{
+  $original = get_post_meta(get_the_ID(), 'syndication_permalink', true);
+  if ($original != "" && $canonical != $original) {
+    return $original;
+  } else {
+    return $canonical;
+  }
+}
+?>
